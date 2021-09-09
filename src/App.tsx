6f5a0f578274a15a5d5cc11436b68a6ff4fe4419
Sudarshan Jhawar <sudarshan.jhawar@outlook.com>
2021-09-09 10:25:00 +0530

    router : basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import Dashboard from './Pages/Dashboard';
 import './App.css';
-import SignIn from './Pages/Login';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './Pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/" exact={true} component={Login} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,20 +1,16 @@
-import React from "react";
+import { useState } from "react";
 import clsx from "clsx";
 import { makeStyles, Theme } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Drawer from "@material-ui/core/Drawer";
-import Box from "@material-ui/core/Box";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import Badge from "@material-ui/core/Badge";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import Link from "@material-ui/core/Link";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import NotificationsIcon from "@material-ui/icons/Notifications";
@@ -117,15 +113,13 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 const Dashboard = () => {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = useState(true);
   const handleDrawerOpen = () => {
     setOpen(true);
   };
   const handleDrawerClose = () => {
     setOpen(false);
   };
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
-
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -187,12 +181,12 @@ const Dashboard = () => {
             <Grid item xs={12}></Grid>
           </Grid>
         </Container>
-        <footer className={classes.footer}>
-          <Container maxWidth="sm">
-            <Copyright />
-          </Container>
-        </footer>
       </main>
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Copyright />
+        </Container>
+      </footer>
     </div>
   );
 };
